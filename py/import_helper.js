8de7932bf49436814da175a9736027691c549a0a
kiwislob <kiwislob@gmail.com>
2024-10-10 23:48:08 +1300

    rm

--- py/import_helper.js
+++
@@ -1,8 +0,0 @@
-
-import { h, render, Component } from "preact";
-//import GoBoard from "@sabaki/go-board";
-//import { Goban } from "@sabaki/shudan";
-//import html2canvas from "html2canvas-pro";
-//
-//import "@sabaki/shudan/css/goban.css";
-//import "../css/main.css";
